56d8800a9b7a3f060b02964f9bbab514efa5fef3
muyan9 <muyan9@163.com>
2014-03-06 14:22:12 +0800

    补充计算sha1方法
    补充计算md5方法

--- hash.py
+++ hash.py
@@ -2,6 +2,7 @@
 
 import hashlib, os
 import binascii
+import hashlib
 
 
 def md5(str):
@@ -9,7 +10,15 @@ def md5(str):
 
 def md5_file(filename):
     if os.path.exists(filename) and os.path.isfile(filename):
-        f = open(filename, 'rb')
+        hash_new = hashlib.md5() #或hashlib.md5()
+        with open(filename,'rb') as fp: #打开文件，一定要以二进制打开
+            while True:
+                data = fp.read(4096*256) #读取文件块
+                if not data: #直到读完文件
+                    break
+                hash_new.update(data)
+        hash_value = hash_new.hexdigest() #生成40位(sha1)或32位(md5)的十六进制字符串
+        return hash_value
         
     else:
         raise Exception("not exist this file : %s." % filename)
@@ -17,9 +26,31 @@ def md5_file(filename):
 def crc32(str):
     return '%08X' % (binascii.crc32(str) & 0xffffffff)
 
-path = u"D:\\迅雷下载\\CentOS-6.5-x86_64-bin-DVD1.iso"
 
-print len(open(path, 'rb').read())
+def sha1(str):
+    hash_new = hashlib.sha1() #或hashlib.md5()
+    hash_new.update(str)
+    hash_value = hash_new.hexdigest() #生成40位(sha1)或32位(md5)的十六进制字符串
+    return hash_value
 
-print md5(open(path, 'rb').read())
-print crc32(open(path, 'rb').read())
+def sha1_file(filename):
+    hash_new = hashlib.sha1() #或hashlib.md5()
+    with open(filename,'rb') as fp: #打开文件，一定要以二进制打开
+        while True:
+            data = fp.read(4096*256) #读取文件块
+            if not data: #直到读完文件
+                break
+            hash_new.update(data)
+    hash_value = hash_new.hexdigest() #生成40位(sha1)或32位(md5)的十六进制字符串
+    return hash_value
+
+
+if __name__ == "__main__":
+    path = u"D:\\迅雷下载\\apache-tomcat-7.0.52.zip"
+    
+    print len(open(path, 'rb').read())
+    print md5(open(path, 'rb').read())
+    print md5_file(path)
+    print crc32(open(path, 'rb').read())
+    print sha1(open(path, 'rb').read())
+    print sha1_file(path)
