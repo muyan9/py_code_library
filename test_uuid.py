76634f1f89ed22c51d7edf2510a7926d2af77d87
zcy <zcy@LAPTOP-HIUH2BHQ>
2018-11-02 10:11:16 +0800

    aa

--- test_uuid.py
+++ test_uuid.py
@@ -2,5 +2,9 @@ import uuid
 
 print uuid.uuid1()
 print uuid.uuid4()
+print uuid.NAMESPACE_DNS
+print uuid.NAMESPACE_OID
+print uuid.NAMESPACE_URL
+print uuid.NAMESPACE_X500
 print uuid.uuid3(uuid.NAMESPACE_DNS,"b")
 print uuid.uuid5(uuid.NAMESPACE_DNS,"b")
